8b4b1905049ef74f23fd26b8c316d89801c09b53
hakohumi <mihumihu32@gmail.com>
2022-03-17 00:22:59 +0900

    ディレクトリ内のファイルの検索、座標に合わせてマーカーを設置

---
+++ exif_photo/Marker.py
@@ -0,0 +1,5 @@
+
+class Marker:
+    def __init__(self, filepath: str, location: tuple[float, float]):
+        self.filepath = filepath
+        self.location = location
--- exif_photo/main.py
+++ exif_photo/main.py
@@ -1,29 +1,60 @@
 from exif_reader import exif_reader as ERead
+from Marker import Marker
 
 import glob
 
 import folium
 
 if __name__ == "__main__":
+    def search_file(search_dir: str) -> list[str]:
+        filepaths = glob.glob(search_dir)
+        if not filepaths:
+            print(f"{search_dir}は画像ファイルが存在しません。")
+            return []
+        return filepaths
+
     def _main():
-        exif_image = ERead.ExifImage("image.jpg")
-        exif_image.print()
-        exif_image.print_geo()
-        location = exif_image.get_geo_deg()
+        filepaths = search_file("./images/**/*.png")
+        filepaths.extend(search_file("./images/**/*.jpg"))
+
+        markers: list[Marker] = []
+
+        for path in filepaths:
+            exif_image = ERead.ExifImage(path)
+            exif_image.print()
+            exif_image.print_geo()
+
+            location = exif_image.get_geo_deg()
+            if location is None:
+                continue
+
+            marker = Marker(path, location)
+            markers.append(marker)
 
         map = folium.Map(
-            location=location,
+            location=markers[0].location,
             zoom_start=20)
 
-        # ポップアップの作成(「show=True」で常に表示)
-        p_up = folium.Popup('写真のジオタグ', min_width=0, max_width=1000, show=True)
+        print()
 
-        # 地図オブジェクトにプロット
-        folium.Marker(
-            location=location,
-            popup=p_up).add_to(map)
+        for marker in markers:
+            filepath = marker.filepath[-10:-1]
+            print(filepath)
+            # ポップアップの作成(「show=True」で常に表示)
+            p_up = folium.Popup(
+                filepath,
+                min_width=0,
+                max_width=1000,
+                show=True)
+
+            # 地図オブジェクトにプロット
+            folium.Marker(
+                location=marker.location,
+                popup=p_up).add_to(map)
 
         # 地図表示
         map.save('index.html')
+
+        print("finish")
 
     _main()
