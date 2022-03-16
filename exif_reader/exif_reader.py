1a3f7f0f84e0456f9eeb2a77226db057a7fe1191
hakohumi <mihumihu32@gmail.com>
2022-03-16 17:49:44 +0900

    add: GPS情報からマップに表示する機能

--- exif_reader/exif_reader.py
+++ exif_reader/exif_reader.py
@@ -40,9 +40,55 @@ class ExifImage:
 
         print(location)
 
+    def print_geo_deg(self):
+        if self.exif.get("GPSInfo", None) is None:
+            print("GPSInfoがありません。")
+            return
+
+        gps = self.exif["GPSInfo"]
+        print(calc_deg(gps))
+
 
 def getpos(dr, value):
     d = int(value[0])
     m = int(value[1])
     s = value[2]
     return f"{d}°{m}'" + f'{s}"{dr}'  # 'を含む文字列を""で "を含む文字列を''で囲む
+
+
+def calc_deg(gps_info: dict):
+    if not("GPSLatitude" in gps_info and
+            "GPSLatitudeRef" in gps_info and
+            "GPSLongitude" in gps_info and
+            "GPSLongitudeRef" in gps_info):
+        print("GPSInfo内に緯度経度情報がありません。")
+        return None
+
+    lat = gps_info['GPSLatitude']
+    lat_ref = gps_info['GPSLatitudeRef']
+    # GPS情報から経度に関する情報を取り出す
+    lon = gps_info['GPSLongitude']
+    lon_ref = gps_info['GPSLongitudeRef']
+
+
+# 北緯の場合プラス、南緯の場合マイナスを設定
+    if lat_ref == 'N':
+        lat_sign = 1.0
+    elif lat_ref == 'S':
+        lat_sign = -1.0
+    else:
+        return None
+
+# 東経の場合プラス、西経の場合マイナスを設定
+    if lon_ref == 'E':
+        lon_sign = 1.0
+    elif lon_ref == 'W':
+        lon_sign = -1.0
+    else:
+        return None
+
+# 度分秒 を 十進経緯度 に変換する
+    lat_ang0 = lat_sign * lat[0] + lat[1] / 60 + lat[2] / 3600
+    lon_ang0 = lon_sign * lon[0] + lon[1] / 60 + lon[2] / 3600
+# コンマ区切りで１つにまとめる
+    return str(lat_ang0) + ', ' + str(lon_ang0)
